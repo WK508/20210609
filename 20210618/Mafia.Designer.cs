ccb5c2203e14efe9d79616b22f07e9a1a8437717
WK508 <75110030+WK508@users.noreply.github.com>
2021-06-18 20:10:08 +0900

    2021 06 18
    2021 06 18

---
+++ 20210618/Mafia.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace _20210618
+{
+    partial class Mafia
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label_main = new System.Windows.Forms.Label();
+            this.label_object = new System.Windows.Forms.Label();
+            this.label_result = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(173, 77);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "마피아";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(300, 77);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "경찰";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(437, 77);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "시민";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(260, 151);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "사격 주체 : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(260, 207);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "사격 대상 : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(260, 264);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "사격 여부 : ";
+            // 
+            // label_main
+            // 
+            this.label_main.AutoSize = true;
+            this.label_main.Location = new System.Drawing.Point(364, 151);
+            this.label_main.Name = "label_main";
+            this.label_main.Size = new System.Drawing.Size(38, 12);
+            this.label_main.TabIndex = 6;
+            this.label_main.Text = "label4";
+            // 
+            // label_object
+            // 
+            this.label_object.AutoSize = true;
+            this.label_object.Location = new System.Drawing.Point(364, 207);
+            this.label_object.Name = "label_object";
+            this.label_object.Size = new System.Drawing.Size(38, 12);
+            this.label_object.TabIndex = 7;
+            this.label_object.Text = "label5";
+            // 
+            // label_result
+            // 
+            this.label_result.AutoSize = true;
+            this.label_result.Location = new System.Drawing.Point(364, 264);
+            this.label_result.Name = "label_result";
+            this.label_result.Size = new System.Drawing.Size(38, 12);
+            this.label_result.TabIndex = 8;
+            this.label_result.Text = "label6";
+            // 
+            // Mafia
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(692, 384);
+            this.Controls.Add(this.label_result);
+            this.Controls.Add(this.label_object);
+            this.Controls.Add(this.label_main);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Mafia";
+            this.Text = "Mafia";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label_main;
+        private System.Windows.Forms.Label label_object;
+        private System.Windows.Forms.Label label_result;
+    }
+}
+
---
+++ 20210618/Mafia.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20210618
+{
+    enum mafia
+    {
+        마피아, 경찰, 시민
+    }
+
+    public partial class Mafia : Form
+    {
+        public Mafia()
+        {
+            InitializeComponent();
+
+            button1.Text = "마피아";
+            button2.Text = "경찰";
+            button3.Text = "시민";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int mychoice = 0;
+            int pcchoice = new Random().Next(3);
+            label_main.Text = "마피아";
+
+            if (pcchoice == 0)
+            {
+                label_object.Text = "마피아";
+                label_result.Text = "사격 중지";
+                MessageBox.Show("같은 마피아이기 때문에 사격하지 않는다.");
+            }
+
+            else if (pcchoice == 1)
+            {
+                label_object.Text = "경찰";
+                label_result.Text = "사격 개시";
+            }
+
+            else
+            {
+                label_object.Text = "시민";
+                label_result.Text = "사격 개시";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int mychoice = 1;
+            int pcchoice = new Random().Next(3);
+            label_main.Text = "경찰";
+
+            if (pcchoice == 0)
+            {
+                label_object.Text = "마피아";
+                label_result.Text = "사격 개시";
+            }
+
+            else if (pcchoice == 1)
+            {
+                label_object.Text = "경찰";
+                label_result.Text = "사격 중지";
+                MessageBox.Show("같은 경찰이기 때문에 사격하지 않는다.");
+            }
+
+            else
+            {
+                label_object.Text = "시민";
+                label_result.Text = "사격 중지";
+                MessageBox.Show("시민에게는 사격하지 않는 것이 원칙이다.");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int mychoice = 2;
+            int pcchoice = new Random().Next(3);
+            label_main.Text = "시민";
+
+            if (pcchoice == 0)
+            {
+                label_object.Text = "마피아";
+                label_result.Text = "사격 개시";
+            }
+
+            else if (pcchoice == 1)
+            {
+                label_object.Text = "경찰";
+                label_result.Text = "사격 중지";
+                MessageBox.Show("경찰에게는 사격하지 않는 것이 원칙이다.");
+            }
+
+            else
+            {
+                label_object.Text = "시민";
+                label_result.Text = "사격 중지";
+                MessageBox.Show("같은 시민이기 때문에 사격하지 않는다.");
+            }
+        }
+    }
+
+
+}
