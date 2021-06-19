ea55dfb737632975369b5bd29bca13c29f4f866a
WK508 <75110030+WK508@users.noreply.github.com>
2021-06-19 22:58:44 +0900

    2021 06 19_1
    2021 06 19_1

---
+++ 20210619_1/DBDBDIP.Designer.cs
@@ -0,0 +1,160 @@
+﻿
+namespace _20210619_1
+{
+    partial class DBDBDIP
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.defense = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.offense = new System.Windows.Forms.Label();
+            this.result = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(309, 82);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "부리";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(463, 82);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "날개";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(239, 156);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "공격 : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(239, 276);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "결과 : ";
+            // 
+            // defense
+            // 
+            this.defense.AutoSize = true;
+            this.defense.Location = new System.Drawing.Point(393, 214);
+            this.defense.Name = "defense";
+            this.defense.Size = new System.Drawing.Size(38, 12);
+            this.defense.TabIndex = 7;
+            this.defense.Text = "label5";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(239, 214);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "수비 : ";
+            // 
+            // offense
+            // 
+            this.offense.AutoSize = true;
+            this.offense.Location = new System.Drawing.Point(393, 156);
+            this.offense.Name = "offense";
+            this.offense.Size = new System.Drawing.Size(38, 12);
+            this.offense.TabIndex = 10;
+            this.offense.Text = "label4";
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.Location = new System.Drawing.Point(393, 276);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(38, 12);
+            this.result.TabIndex = 11;
+            this.result.Text = "label6";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(153, 82);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "주먹";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // DBDBDIP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(730, 382);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.offense);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.defense);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Name = "DBDBDIP";
+            this.Text = "디비디비딥";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label defense;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label offense;
+        private System.Windows.Forms.Label result;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ 20210619_1/DBDBDIP.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20210619_1
+{
+    enum dbdbdip
+    {
+        주먹, 부리, 날개
+    }
+
+    public partial class DBDBDIP : Form
+    {
+        public DBDBDIP()
+        {
+            InitializeComponent();
+
+            button1.Text = "주먹";
+            button2.Text = "부리";
+            button3.Text = "날개";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int mychoice = 1;
+            int pcchoice = new Random().Next(3);
+            offense.Text = "부리";
+
+            if (pcchoice == 0)
+            {
+                defense.Text = "주먹";
+                result.Text = "수비성공 and 공격실패";
+            }
+
+            else if (pcchoice == 1)
+            {
+                defense.Text = "부리";
+                result.Text = "수비실패 and 공격성공";
+            }
+
+            else
+            {
+                defense.Text = "날개";
+                result.Text = "수비성공 and 공격실패";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int mychoice = 2;
+            int pcchoice = new Random().Next(3);
+            offense.Text = "날개";
+
+            if (pcchoice == 0)
+            {
+                defense.Text = "주먹";
+                result.Text = "수비성공 and 공격실패";
+            }
+
+            else if (pcchoice == 1)
+            {
+                defense.Text = "부리";
+                result.Text = "수비성공 and 공격실패";
+            }
+
+            else
+            {
+                defense.Text = "날개";
+                result.Text = "수비실패 and 공격성공";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int mychoice = 0;
+            int pcchoice = new Random().Next(3);
+            offense.Text = "주먹";
+
+            if (pcchoice == 0)
+            {
+                defense.Text = "주먹";
+                result.Text = "수비실패 and 공격성공";
+            }
+
+            else if (pcchoice == 1)
+            {
+                defense.Text = "부리";
+                result.Text = "수비성공 and 공격실패";
+            }
+
+            else
+            {
+                defense.Text = "날개";
+                result.Text = "수비성공 and 공격실패";
+            }
+        }
+    }
+}
