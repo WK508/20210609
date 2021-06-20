6f24326cb5bdc12c67438efb55a8dd8dbede0ebc
WK508 <75110030+WK508@users.noreply.github.com>
2021-06-20 22:08:49 +0900

    2021 06 20_2
    2021 06 20_2

---
+++ 20210620_2/RSP.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace _20210620_2
+{
+    partial class RSP
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.resulta = new System.Windows.Forms.Label();
+            this.resultb = new System.Windows.Forms.Label();
+            this.result = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(185, 94);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "가위";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(297, 94);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "바위";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(413, 94);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "보";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(259, 148);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "A : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(258, 192);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "B : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(258, 234);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "결과 : ";
+            // 
+            // resulta
+            // 
+            this.resulta.AutoSize = true;
+            this.resulta.Location = new System.Drawing.Point(361, 147);
+            this.resulta.Name = "resulta";
+            this.resulta.Size = new System.Drawing.Size(38, 12);
+            this.resulta.TabIndex = 6;
+            this.resulta.Text = "label4";
+            // 
+            // resultb
+            // 
+            this.resultb.AutoSize = true;
+            this.resultb.Location = new System.Drawing.Point(361, 191);
+            this.resultb.Name = "resultb";
+            this.resultb.Size = new System.Drawing.Size(38, 12);
+            this.resultb.TabIndex = 7;
+            this.resultb.Text = "label5";
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.Location = new System.Drawing.Point(361, 232);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(38, 12);
+            this.result.TabIndex = 8;
+            this.result.Text = "label6";
+            // 
+            // RSP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.resultb);
+            this.Controls.Add(this.resulta);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "RSP";
+            this.Text = "가위 바위 보";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label resulta;
+        private System.Windows.Forms.Label resultb;
+        private System.Windows.Forms.Label result;
+    }
+}
+
---
+++ 20210620_2/RSP.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20210620_2
+{
+    enum rsp
+    {
+        가위, 바위, 보
+    }
+
+    public partial class RSP : Form
+    {
+        public RSP()
+        {
+            InitializeComponent();
+
+            button1.Text = "가위";
+            button2.Text = "바위";
+            button3.Text = "보";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int mychoice = 0;
+            int pcchoice = new Random().Next(3);
+            resulta.Text = "가위";
+
+            if (pcchoice == 0)
+            {
+                resultb.Text = "가위";
+                result.Text = "무승부";
+            }
+
+            else if (pcchoice == 1)
+            {
+                resultb.Text = "바위";
+                result.Text = "B의 승리";
+            }
+
+            else
+            {
+                resultb.Text = "보";
+                result.Text = "A의 승리";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int mychoice = 1;
+            int pcchoice = new Random().Next(3);
+            resultb.Text = "바위";
+
+            if (pcchoice == 0)
+            {
+                resulta.Text = "가위";
+                result.Text = "B의 승리";
+            }
+
+            else if (pcchoice == 1)
+            {
+                resulta.Text = "바위";
+                result.Text = "무승부";
+            }
+
+            else
+            {
+                resulta.Text = "보";
+                result.Text = "A의 승리";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int mychoice = 3;
+            int pcchoice = new Random().Next(3);
+            resulta.Text = "보";
+
+            if (pcchoice == 0)
+            {
+                resultb.Text = "가위";
+                result.Text = "B의 승리";
+            }
+
+            else if (pcchoice == 1)
+            {
+                resultb.Text = "바위";
+                result.Text = "A의 승리";
+            }
+
+            else
+            {
+                resultb.Text = "보";
+                result.Text = "무승부";
+            }
+        }
+    }
+}
