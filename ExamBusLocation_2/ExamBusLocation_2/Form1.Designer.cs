a6a4b42f80f84539457fd6aa05c7fb786ea2c6e4
WK508 <75110030+WK508@users.noreply.github.com>
2021-07-06 21:26:26 +0900

    2021 06 29
    ExamBusLocation_2

---
+++ ExamBusLocation_2/ExamBusLocation_2/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace ExamBusLocation_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("NO.");
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem("버스번호");
+            System.Windows.Forms.ListViewItem listViewItem3 = new System.Windows.Forms.ListViewItem("막차여부");
+            System.Windows.Forms.ListViewItem listViewItem4 = new System.Windows.Forms.ListViewItem("빈자리 수");
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.listViewResults = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.BackColor = System.Drawing.Color.Aqua;
+            this.buttonRefresh.Location = new System.Drawing.Point(560, 99);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(168, 79);
+            this.buttonRefresh.TabIndex = 1;
+            this.buttonRefresh.Text = "업데이트";
+            this.buttonRefresh.UseVisualStyleBackColor = false;
+            this.buttonRefresh.Click += new System.EventHandler(this.buttonRefresh_Click);
+            // 
+            // listViewResults
+            // 
+            this.listViewResults.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.listViewResults.HideSelection = false;
+            this.listViewResults.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1,
+            listViewItem2,
+            listViewItem3,
+            listViewItem4});
+            this.listViewResults.Location = new System.Drawing.Point(138, 81);
+            this.listViewResults.Name = "listViewResults";
+            this.listViewResults.Size = new System.Drawing.Size(416, 255);
+            this.listViewResults.TabIndex = 2;
+            this.listViewResults.UseCompatibleStateImageBehavior = false;
+            this.listViewResults.View = System.Windows.Forms.View.SmallIcon;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listViewResults);
+            this.Controls.Add(this.buttonRefresh);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonRefresh;
+        private System.Windows.Forms.ListView listViewResults;
+    }
+}
+
---
+++ ExamBusLocation_2/ExamBusLocation_2/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace ExamBusLocation_2
+{
+    public partial class Form1 : Form
+    {
+        const string targetURL = "http://openapi.gbis.go.kr/ws/rest/buslocationservice";
+        const string serviceKey = "bLORjXNAfn0T3bvdmfewxXjUZllXmuQzSvIrA9bEOATdGD%2FNsMpHeRNAlno9VUBv4pvL4psVP3fPLXmmJ4BXxg=="; //마이 페이지에서 자신의 인증키 입력.
+        const string routeid = "233000031"; //버스 노선 ID
+
+		public static string getResults()
+		{
+			string result = string.Empty;
+
+			try
+			{
+				WebClient client = new WebClient();
+				string url = string.Format(@"{0}?serviceKey={1}&routeId={2}", targetURL, serviceKey, routeid);
+				using (Stream data = client.OpenRead(url))
+				{
+					using (StreamReader reader = new StreamReader(data))
+					{
+						string s = reader.ReadToEnd();
+						result = s;
+
+						reader.Close();
+						data.Close();
+					}
+				}
+			}
+			catch (Exception exc)
+			{
+				MessageBox.Show(exc.Message);
+			}
+
+			return result;
+		}
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void buttonRefresh_Click(object sender, EventArgs e)
+        {
+			string result = getResults();
+
+			XmlDocument xml = new XmlDocument();
+			xml.LoadXml(result);
+			XmlNodeList list = xml.GetElementsByTagName("busLocationList");
+
+			listViewResults.Items.Clear();
+			int idx = 0;
+			foreach (XmlNode bus in list)
+			{
+				ListViewItem item = new ListViewItem((idx++).ToString());
+				item.SubItems.Add(bus["plateNo"].InnerText);
+				string endBus = "N";
+				if (bus["endBus"].InnerText == "1")
+					endBus = "Y";
+				item.SubItems.Add(endBus);
+
+				item.SubItems.Add(bus["remainSeatCnt"].InnerText);
+
+				listViewResults.Items.Add(item);
+			}
+		}  
+    }
+    }
+
+
+
+    
+
