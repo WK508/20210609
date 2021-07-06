96587988e7250d2abcac2805a73ce74a8b9d345a
WK508 <75110030+WK508@users.noreply.github.com>
2021-07-06 21:30:14 +0900

    2021 07 01
    covid19_infection

---
+++ covid19_infection/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿
+namespace covid19_infection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column12 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column13 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column14 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column15 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column16 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column17 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column18 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column19 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column8,
+            this.column7,
+            this.Column9,
+            this.Column10,
+            this.Column11,
+            this.Column12,
+            this.Column13,
+            this.Column14,
+            this.Column1,
+            this.Column2,
+            this.Column15,
+            this.Column4,
+            this.Column3,
+            this.Column16,
+            this.Column17,
+            this.Column18,
+            this.Column19,
+            this.Column6,
+            this.Column5});
+            this.dataGridView1.Location = new System.Drawing.Point(37, 30);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(713, 393);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "결과메시지";
+            this.Column8.Name = "Column8";
+            // 
+            // column7
+            // 
+            this.column7.HeaderText = "결과코드";
+            this.column7.Name = "column7";
+            this.column7.Width = 150;
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "한 페이지 결과 수";
+            this.Column9.Name = "Column9";
+            this.Column9.Width = 150;
+            // 
+            // Column10
+            // 
+            this.Column10.HeaderText = "페이지 번호";
+            this.Column10.Name = "Column10";
+            // 
+            // Column11
+            // 
+            this.Column11.HeaderText = "전체 결과 수";
+            this.Column11.Name = "Column11";
+            // 
+            // Column12
+            // 
+            this.Column12.HeaderText = "게시글 번호(감염현황 고유값)";
+            this.Column12.Name = "Column12";
+            this.Column12.Width = 200;
+            // 
+            // Column13
+            // 
+            this.Column13.HeaderText = "기준일";
+            this.Column13.Name = "Column13";
+            // 
+            // Column14
+            // 
+            this.Column14.HeaderText = "기준시간";
+            this.Column14.Name = "Column14";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "확진자 수";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "격리해제 수";
+            this.Column2.Name = "Column2";
+            // 
+            // Column15
+            // 
+            this.Column15.HeaderText = "검사진행 수";
+            this.Column15.Name = "Column15";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "사망자 수";
+            this.Column4.Name = "Column4";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "치료중 환자 수";
+            this.Column3.Name = "Column3";
+            // 
+            // Column16
+            // 
+            this.Column16.HeaderText = "결과 음성 수";
+            this.Column16.Name = "Column16";
+            // 
+            // Column17
+            // 
+            this.Column17.HeaderText = "누적 검사 수";
+            this.Column17.Name = "Column17";
+            // 
+            // Column18
+            // 
+            this.Column18.HeaderText = "누적 검사 완료 수";
+            this.Column18.Name = "Column18";
+            this.Column18.Width = 150;
+            // 
+            // Column19
+            // 
+            this.Column19.HeaderText = "누적 확진률";
+            this.Column19.Name = "Column19";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "등록일시분초";
+            this.Column6.Name = "Column6";
+            this.Column6.Width = 150;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "수정일시분초";
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 150;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column10;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column11;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column12;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column13;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column14;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column15;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column16;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column17;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column18;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column19;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+    }
+}
+
---
+++ covid19_infection/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System.Net;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace covid19_infection
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            WebClient wc = null;
+            XmlDocument doc = null;
+
+            wc = new WebClient()
+            {
+                Encoding = Encoding.UTF8
+            };
+            doc = new XmlDocument();
+
+            StringBuilder str = new StringBuilder();
+            str.Append("http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson");
+            str.Append("?bLORjXNAfn0T3bvdmfewxXjUZllXmuQzSvIrA9bEOATdGD%2FNsMpHeRNAlno9VUBv4pvL4psVP3fPLXmmJ4BXxg==");
+            str.Append("&startCreateDt=");
+            str.Append("20200310");
+            str.Append("&endCreateDt=");
+            str.Append("20200315");
+
+            string xml = wc.DownloadString(str.ToString());
+            doc.LoadXml(xml);
+            XmlElement root = doc.DocumentElement;
+            XmlNodeList xnList = doc.GetElementsByTagName("item");
+
+            dataGridView1.Rows.Clear();
+
+            foreach (XmlNode xn in xnList)
+            {
+                dataGridView1.Rows.Add(
+                    xn["careCnt"].InnerText,
+                    xn["clearCnt"].InnerText,
+                    xn["createDt"].InnerText,
+                    xn["deathCnt"].InnerText,
+                    xn["decideCnt"].InnerText,
+                    xn["examCnt"].InnerText,
+                    xn["resutlNegCnt"].InnerText,
+                    xn["seq"].InnerText,
+                    xn["stateDt"].InnerText,
+                    xn["stateTime"].InnerText);
+            }
+        }
+    }
+}
