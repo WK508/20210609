ca28c816f1ffea6c8529a645469b198b4fec9047
WK508 <75110030+WK508@users.noreply.github.com>
2021-07-21 22:31:53 +0900

    2021 07 21
    Panel

---
+++ Panel/Form1.cs
@@ -0,0 +1,56 @@
+﻿using Panel.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Panel
+{
+    public partial class Form1 : Form
+    {
+        private bool isCollapsed;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (isCollapsed)
+            {
+                button5.Image = Resources.icons8_c_sharp_logo_32px;
+                panelDropDown.Height += 10;
+                if (panelDropDown.Size == panelDropDown.MaximumSize)
+                {
+                    timer1.Stop();
+                    isCollapsed = false;
+                }
+            }
+            else
+            {
+                button5.Image = Resources.icons8_shutdown_48px;
+                panelDropDown.Height -= 10;
+                if (panelDropDown.Size == panelDropDown.MinimumSize)
+                {
+                    timer1.Stop();
+                    isCollapsed = true;
+                }
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+    }
+}
